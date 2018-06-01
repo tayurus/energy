e7b7bb4203c6673f8f2b3211700062fc79d3cb06
Yury Tatarintsev <yury@realrussialimited.local>
2018-06-01 17:11:05 +0300

    add header-menu.

---
+++ blocks/article-slider/article-slider.js
@@ -0,0 +1,6 @@
+$('.article-slider').owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ["<img src='img/arrow-left.png'>","<img src='img/arrow-right.png'>"],
+    dots: false
+})
---
+++ blocks/header/header.js
@@ -0,0 +1,7 @@
+$('.header__menu-icon').click(function(){
+    $(".header-menu").addClass("header-menu_active");
+})
+
+$(".header-menu").click(function(){
+    $(".header-menu").removeClass("header-menu_active");
+})
